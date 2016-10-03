4df5f2fcbf617256fb8975917151875aaa5341c8
Henrique Dias <heukirne@gmail.com>
2016-10-03 10:29:52 -0300

    add pr hs len combination

---
+++ NDCG.py
@@ -0,0 +1,141 @@
+import numpy as np
+import sys
+import types
+
+def reload_package(root_module):
+    package_name = root_module.__name__
+
+    # get a reference to each loaded module
+    loaded_package_modules = dict([
+        (key, value) for key, value in sys.modules.items() 
+        if key.startswith(package_name) and isinstance(value, types.ModuleType)])
+
+    # delete references to these loaded modules from sys.modules
+    for key in loaded_package_modules:
+        del sys.modules[key]
+
+    # load each of the modules again; 
+    # make old modules share state with new modules
+    for key in loaded_package_modules:
+        print 'loading %s' % key
+        newmodule = __import__(key)
+        oldmodule = loaded_package_modules[key]
+        oldmodule.__dict__.clear()
+        oldmodule.__dict__.update(newmodule.__dict__)
+        
+
+def dcg_at_k(r, k, method=0):
+    """Score is discounted cumulative gain (dcg)
+    Relevance is positive real values.  Can use binary
+    as the previous methods.
+    Example from
+    http://www.stanford.edu/class/cs276/handouts/EvaluationNew-handout-6-per.pdf
+    >>> r = [3, 2, 3, 0, 0, 1, 2, 2, 3, 0]
+    >>> dcg_at_k(r, 1)
+    3.0
+    >>> dcg_at_k(r, 1, method=1)
+    3.0
+    >>> dcg_at_k(r, 2)
+    5.0
+    >>> dcg_at_k(r, 2, method=1)
+    4.2618595071429155
+    >>> dcg_at_k(r, 10)
+    9.6051177391888114
+    >>> dcg_at_k(r, 11)
+    9.6051177391888114
+    Args:
+        r: Relevance scores (list or numpy) in rank order
+            (first element is the first item)
+        k: Number of results to consider
+        method: If 0 then weights are [1.0, 1.0, 0.6309, 0.5, 0.4307, ...]
+                If 1 then weights are [1.0, 0.6309, 0.5, 0.4307, ...]
+    Returns:
+        Discounted cumulative gain
+    """
+    r = np.asfarray(r)[:k]
+    if r.size:
+        if method == 0:
+            return r[0] + np.sum(r[1:] / np.log2(np.arange(2, r.size + 1)))
+        elif method == 1:
+            return np.sum(r / np.log2(np.arange(2, r.size + 2)))
+        else:
+            raise ValueError('method must be 0 or 1.')
+    return 0.
+
+
+def ndcg_at_k(r, k, method=0):
+    """Score is normalized discounted cumulative gain (ndcg)
+    Relevance is positive real values.  Can use binary
+    as the previous methods.
+    Example from
+    http://www.stanford.edu/class/cs276/handouts/EvaluationNew-handout-6-per.pdf
+    >>> r = [3, 2, 3, 0, 0, 1, 2, 2, 3, 0]
+    >>> ndcg_at_k(r, 1)
+    1.0
+    >>> r = [2, 1, 2, 0]
+    >>> ndcg_at_k(r, 4)
+    0.9203032077642922
+    >>> ndcg_at_k(r, 4, method=1)
+    0.96519546960144276
+    >>> ndcg_at_k([0], 1)
+    0.0
+    >>> ndcg_at_k([1], 2)
+    1.0
+    Args:
+        r: Relevance scores (list or numpy) in rank order
+            (first element is the first item)
+        k: Number of results to consider
+        method: If 0 then weights are [1.0, 1.0, 0.6309, 0.5, 0.4307, ...]
+                If 1 then weights are [1.0, 0.6309, 0.5, 0.4307, ...]
+    Returns:
+        Normalized discounted cumulative gain
+    """
+    dcg_max = dcg_at_k(sorted(r, reverse=True), k, method)
+    if not dcg_max:
+        return 0.
+    return dcg_at_k(r, k, method) / dcg_max
+
+
+
+def calc_ndcg(df, column,k):
+    min_votes=5
+    min_comments=30
+
+    ndcg_global=[]
+
+
+    grouped=df[df['tot'].astype(int)>min_votes].groupby('asin')
+
+    for name, group in grouped:
+        dffiltro = (df['asin']==name) & (df['tot'].astype(int)>min_votes)
+
+        comments_count = df[dffiltro ]['tot'].values
+        if ( (len(comments_count)>min_comments) ):
+
+            values_test = df[dffiltro]['helpfulness'].values
+            scores = df[dffiltro][column].values
+
+
+            ind = (-np.array(scores)).argsort()
+            a = np.array(values_test)[ind]
+            ndcg = ndcg_at_k(a, k)
+            
+            #print "product="+str(name)+" ndcg="+str(ndcg)
+            ndcg_global.append(ndcg)
+    return ndcg_global
+
+def calc_ndcg_mean(df, column,k):
+    x = calc_ndcg(df,column,k)
+    return np.mean(x)
+
+def calc_corr(df, column):
+    correlation=[]
+    grouped=df.groupby('asin')
+
+    for name, group in grouped:
+        dffiltro = (df['asin']==name)
+        
+        helpfulness = df[dffiltro]['helpfulness'].T.to_dict().values()
+        scores = df[dffiltro][column].T.to_dict().values()
+        correlation.append(np.corrcoef(helpfulness,scores)[0][1])
+    return correlation
